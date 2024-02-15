b7789eeba4161db362a447ab222008ddc76f550a
Zachary Lam <zachary.lam@ucalgary.ca>
2024-02-15 01:31:10 -0700

    created DisasterVictim class without exceptions

--- edu/ucalgary/oop/DisasterVictim.java
+++ edu/ucalgary/oop/DisasterVictim.java
@@ -1,4 +1,96 @@
 package edu.ucalgary.oop;
 
 public class DisasterVictim {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String comments;
+    private static int lastSocialID = 0;
+    private int ASSIGNED_SOCIAL_ID;
+    private MedicalRecord[] medicalRecords;
+    private FamilyRelation[] familyConnections;
+    private String ENTRY_DATE;
+    private Supply[] personalBelongings;
+    private String gender;
+
+    public DisasterVictim(String firstName, String ENTRY_DATE) {
+        this.setFirstName(firstName);
+        this.ENTRY_DATE = ENTRY_DATE;
+        this.ASSIGNED_SOCIAL_ID = ++lastSocialID;
+        this.medicalRecords = new MedicalRecord[0];
+        this.familyConnections = new FamilyRelation[0];
+        this.personalBelongings = new Supply[0];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public MedicalRecord[] getMedicalRecords() {
+        return medicalRecords;
+    }
+
+    public String getEntryDate() {
+        return ENTRY_DATE;
+    }
+
+    public int getAssignedSocialID() {
+        return ASSIGNED_SOCIAL_ID;
+    }
+
+    public Supply[] getPersonalBelongings() {
+        return personalBelongings;
+    }
+
+    public FamilyRelation[] getFamilyConnections() {
+        return familyConnections;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public void setMedicalRecords(MedicalRecord[] medicalRecords) {
+        this.medicalRecords = medicalRecords;
+    }
+
+    public void setPersonalBelongings(Supply[] personalBelongings) {
+        this.personalBelongings = personalBelongings;
+    }
+
+    public void setFamilyConnections(FamilyRelation[] familyConnections) {
+        this.familyConnections = familyConnections;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
