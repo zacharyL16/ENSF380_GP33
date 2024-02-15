eb9d741ba9ba39a5797999dd61407cef9ebdad6b
Issygaudet <isabelle.gaudet@ucalgary.ca>
2024-02-15 13:12:49 -0700

    change code to avoid arraylist

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -42,17 +42,48 @@ public class Location {
         this.supplies = supplies;
     }
     public void addOccupant(DisasterVictim occupant) {
-        this.occupants.add(occupant);
+        DisasterVictim[] newOccupants = new DisasterVictim[occupants.length + 1];
+        System.arraycopy(occupants, 0, newOccupants, 0, occupants.length);
+        newOccupants[occupants.length] = occupant;
+        occupants = newOccupants;
     }
+
     public void removeOccupant(DisasterVictim occupant) {
-        this.occupants.remove(occupant);
+        int index = -1;
+        for (int i = 0; i < occupants.length; i++) {
+            if (occupants[i].equals(occupant)) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            DisasterVictim[] newOccupants = new DisasterVictim[occupants.length - 1];
+            System.arraycopy(occupants, 0, newOccupants, 0, index);
+            System.arraycopy(occupants, index + 1, newOccupants, index, occupants.length - index - 1);
+            occupants = newOccupants;
+        }
     }
 
     public void addSupply(Supply supply) {
-        this.supplies.add(supply);
+        Supply[] newSupplies = new Supply[supplies.length + 1];
+        System.arraycopy(supplies, 0, newSupplies, 0, supplies.length);
+        newSupplies[supplies.length] = supply;
+        supplies = newSupplies;
     }
 
     public void removeSupply(Supply supply) {
-        this.supplies.remove(supply);
+        int index = -1;
+        for (int i = 0; i < supplies.length; i++) {
+            if (supplies[i].equals(supply)) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            Supply[] newSupplies = new Supply[supplies.length - 1];
+            System.arraycopy(supplies, 0, newSupplies, 0, index);
+            System.arraycopy(supplies, index + 1, newSupplies, index, supplies.length - index - 1);
+            supplies = newSupplies;
+        }
     }
 }
