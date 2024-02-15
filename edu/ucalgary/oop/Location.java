bc12e2c90af1edbe9010c00a3c6df12a2ac7e26b
Issygaudet <isabelle.gaudet@ucalgary.ca>
2024-02-15 12:54:57 -0700

    write basic structure of location and supply class

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,4 +1,58 @@
 package edu.ucalgary.oop;
 
 public class Location {
+    private String name;
+    private String address;
+    private DisasterVictim[] occupants;
+    private Supply[] supplies;
+
+    public Location(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public DisasterVictim[] getOccupants() {
+        return occupants;
+    }
+
+    public void setOccupants(DisasterVictim[] occupants) {
+        this.occupants = occupants;
+    }
+
+    public Supply[] getSupplies() {
+        return supplies;
+    }
+
+    public void setSupplies(Supply[] supplies) {
+        this.supplies = supplies;
+    }
+    public void addOccupant(DisasterVictim occupant) {
+        this.occupants.add(occupant);
+    }
+    public void removeOccupant(DisasterVictim occupant) {
+        this.occupants.remove(occupant);
+    }
+
+    public void addSupply(Supply supply) {
+        this.supplies.add(supply);
+    }
+
+    public void removeSupply(Supply supply) {
+        this.supplies.remove(supply);
+    }
 }
--- edu/ucalgary/oop/Supply.java
+++ edu/ucalgary/oop/Supply.java
@@ -1,4 +1,27 @@
 package edu.ucalgary.oop;
 
 public class Supply {
+    private String type;
+    private int quantity;
+
+    public Supply(String type, int quantity) {
+        this.type = type;
+        this.quantity = quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
