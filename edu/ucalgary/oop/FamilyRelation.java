be8fe23ded478f420620490d2e27c1178e2d2a4d
muhammadzain03 <144840339+muhammadzain03@users.noreply.github.com>
2024-02-16 01:32:37 -0700

    Update FamilyRelation.java

--- edu/ucalgary/oop/FamilyRelation.java
+++ edu/ucalgary/oop/FamilyRelation.java
@@ -1,4 +1,42 @@
+// FamilyRelation Code file
+
+
 package edu.ucalgary.oop;
 
 public class FamilyRelation {
+    private DisasterVictim personOne;
+    private DisasterVictim personTwo;
+    private String relationshipTo;
+
+
+    public FamilyRelation(DisasterVictim personOne, String relationshipTo, DisasterVictim personTwo) {
+        this.personOne = personOne;
+        this.relationshipTo = relationshipTo;
+        this.personTwo = personTwo;
+    }
+
+    
+    public DisasterVictim getPersonOne() {
+        return personOne;
+    }
+    
+    public void setPersonOne(DisasterVictim personOne) {
+        this.personOne = personOne;
+    }
+
+    public DisasterVictim getPersonTwo() {
+        return personTwo;
+    }
+
+    public void setPersonTwo(DisasterVictim personTwo) {
+        this.personTwo = personTwo;
+    }
+
+    public String getRelationshipTo() {
+        return relationshipTo;
+    }
+
+    public void setRelationshipTo(String relationshipTo) {
+        this.relationshipTo = relationshipTo;
+    }
 }
