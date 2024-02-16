a6b3471965b0c31974d93ec596454e8868776e3b
Farha Ahmed <farha.ahmed1@ucalgary.ca>
2024-02-16 10:26:26 -0700

    medicalrecord

--- edu/ucalgary/oop/MedicalRecord.java
+++ edu/ucalgary/oop/MedicalRecord.java
@@ -1,4 +1,45 @@
 package edu.ucalgary.oop;
 
 public class MedicalRecord {
+    private Location location; 
+    private String treatmentDetails;
+    private String dateOfTreatment; 
+
+    public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment) throws IllegalArgumentException{
+        String regex = "\\d{4}-\\d{2}-\\d{2}";
+        if (!dateOfTreatment.matches(regex)) {
+            throw new IllegalArgumentException("Invalid date format. Expected format is YYYY-MM-DD.");
+        }
+        this.location = location;
+        this.treatmentDetails = new String(treatmentDetails);
+        this.dateOfTreatment = new String(dateOfTreatment);
+    }
+ 
+    public Location getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getTreatmentDetails() {
+        return this.treatmentDetails;
+    }
+
+    public void setTreatmentDetails(String treatmentDetails) {
+        this.treatmentDetails = new String(treatmentDetails);
+    }
+
+    public String getDateOfTreatment() {
+        return this.dateOfTreatment;
+    }
+
+    public void setDateOfTreatment(String dateOfTreatment) {
+        String regex = "\\d{4}-\\d{2}-\\d{2}";
+    if (!dateOfTreatment.matches(regex)) {
+        throw new IllegalArgumentException("Invalid date format. Expected format is YYYY-MM-DD.");
+    }
+        this.dateOfTreatment = new String(dateOfTreatment);
+    }
 }
