dd3592b48d7dfda84dad0105a64e99bf4afb5388
muhammadzain03 <144840339+muhammadzain03@users.noreply.github.com>
2024-02-16 01:33:27 -0700

    Update ReliefService.java

--- edu/ucalgary/oop/ReliefService.java
+++ edu/ucalgary/oop/ReliefService.java
@@ -1,4 +1,75 @@
+// ReliefService Code File
+
+
 package edu.ucalgary.oop;
 
 public class ReliefService {
+    private Inquirer inquirer;
+    private DisasterVictim missingPerson;
+    private String dateOfInquiry;
+    private String infoProvided;
+    private Location lastKnownLocation;
+    private String logDetails;
+    private static final String regex = "\\d{4}-\\d{2}-\\d{2}";
+
+    
+    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+        this.inquirer = inquirer;
+        this.missingPerson = missingPerson;
+        this.dateOfInquiry = dateOfInquiry;
+        this.infoProvided = infoProvided;
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+
+    public Inquirer getInquirer() {
+        return inquirer;
+    }
+
+    public void setInquirer(Inquirer inquirer) {
+        this.inquirer = inquirer;
+    }
+
+    public DisasterVictim getMissingPerson() {
+        return missingPerson;
+    }
+
+    public void setMissingPerson(DisasterVictim missingPerson) {
+        this.missingPerson = missingPerson;
+    }
+
+    public String getDateOfInquiry() {
+        return dateOfInquiry;
+    }
+
+    public void setDateOfInquiry(String dateOfInquiry) {
+        if (dateOfInquiry.matches(regex)) {
+            this.dateOfInquiry = dateOfInquiry;
+        } 
+        else {
+            throw new IllegalArgumentException("Inquiry date must be in the format YYYY-MM-DD");
+        }
+    }    
+
+    public String getInfoProvided() {
+        return infoProvided;
+    }
+
+    public void setInfoProvided(String infoProvided) {
+        this.infoProvided = infoProvided;
+    }
+
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    public void setLastKnownLocation(Location lastKnownLocation) {
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    public String getLogDetails(){
+        logDetails = String.format("Inquirer: %s, Missing Person: %s, Date of Inquiry: %s, Info Provided: %s, Last Known Location: %s",
+        this.inquirer.getFirstName(), this.missingPerson.getFirstName(), this.dateOfInquiry, this.infoProvided, this.lastKnownLocation.getName());
+        return logDetails;
+    }
 }
