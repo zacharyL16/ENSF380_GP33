3ba3ec6b4441d7e6967631136f06fb379096b7d0
Farha Ahmed <farha.ahmed1@ucalgary.ca>
2024-02-16 08:13:00 -0700

    inquirer

--- edu/ucalgary/oop/Inquirer.java
+++ edu/ucalgary/oop/Inquirer.java
@@ -1,4 +1,21 @@
 package edu.ucalgary.oop;
 
 public class Inquirer {
+
+    private final String FIRST_NAME;
+    private final String LAST_NAME;
+    private final String INFO;
+    private final String SERVICES_PHONE;
+
+    public Inquirer(String firstName, String lastName, String servicesPhone, String info){
+        this.FIRST_NAME = firstName;
+        this.LAST_NAME = lastName;
+        this.SERVICES_PHONE = servicesPhone;
+        this.INFO = info;
+    }
+
+    public String getFirstName() {return this.FIRST_NAME; }
+    public String getLastName() {return this.LAST_NAME; }
+    public String getInfo() {return this.INFO; }
+    public String getServicesPhone() {return this.SERVICES_PHONE; }
 }
